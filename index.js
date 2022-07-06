1529622902045f1c4d9c093b88dba3a2e4bbabe1
Anant Singh <anantsingh7060@gmail.com>
2022-07-06 17:42:11 +0530

    Fixing reset and decimal button

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function sendNumberValue(number) {
     calDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 
 }
+function addDecimal() {
+    if (!calDisplay.textContent.includes('.'))
+        calDisplay.textContent = `${calDisplay.textContent}.`;
+}
 
 //Adding event listerner
 
@@ -16,6 +20,14 @@ inputBtns.forEach((inputBtn) => {
     else if (inputBtn.classList.contains('operator'))
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     else if (inputBtn.classList.contains('decimal'))
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => addDecimal());
 
 });
+
+//Reset Dispplay
+function resetAll() {
+    calDisplay.textContent = '0';
+}
+
+//Event Listener
+clearBtn.addEventListener('click', resetAll);
