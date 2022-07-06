fc98fb5df8bd2ef785615fd5bb222e08f22c63c2
Anant Singh <anantsingh7060@gmail.com>
2022-07-06 18:01:50 +0530

    Fixing operator functionality

--- index.js
+++ index.js
@@ -2,30 +2,62 @@ const calDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.querySelector('#clear-btn');
 
-function sendNumberValue(number) {
-    const displayValue = calDisplay.textContent;
-    calDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
 
+function sendNumberValue(number) {
+    //Replace current display value if first value entered
+    if (awaitingNextValue) {
+        calDisplay.textContent = number;
+        awaitingNextValue = false;
+    }
+    else {
+        const displayValue = calDisplay.textContent;
+        calDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+
+    }
 }
+
 function addDecimal() {
+    //if operatorpressed,dont add decimal
+    if (awaitingNextValue)
+        return;
+    //if no decimal add one
     if (!calDisplay.textContent.includes('.'))
         calDisplay.textContent = `${calDisplay.textContent}.`;
 }
 
+function useOperator(operator) {
+    const currentValue = Number(calDisplay.textContent);
+    //Assign firstValue if no value
+    if (!firstValue)
+        firstValue = currentValue;
+
+    else
+        console.log(currentValue);
+    awaitingNextValue = true;
+    operatorValue = operator;
+    console.log(firstValue);
+    console.log(operatorValue);
+}
 //Adding event listerner
 
 inputBtns.forEach((inputBtn) => {
     if (inputBtn.classList.length === 0)
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     else if (inputBtn.classList.contains('operator'))
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
     else if (inputBtn.classList.contains('decimal'))
         inputBtn.addEventListener('click', () => addDecimal());
 
 });
 
-//Reset Dispplay
+//Reset all Dispplay
 function resetAll() {
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
     calDisplay.textContent = '0';
 }
 
